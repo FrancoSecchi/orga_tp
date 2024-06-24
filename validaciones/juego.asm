2e22130f67609fb6e4b1f810b9e4720120bdb532
FrancoSecchi <francofabiansecchi@gmail.com>
2024-06-24 00:06:45 -0300

    juego validacion

---
+++ validaciones/juego.asm
@@ -0,0 +1,24 @@
+global validar_eleccion_jugada
+
+
+
+section .data
+
+    caracter_salir_juego db "S",0
+
+section .bss
+    caracter_ingresado  resb 1
+
+section .text
+
+validar_eleccion_jugada: 
+    mov al, [rdi]
+    mov [caracter_ingresado], al
+
+    mov al, [caracter_salir_juego]
+    cmp al, [caracter_ingresado]
+    je opcion_finalizar_juego
+
+opcion_finalizar_juego: 
+    mov rax, 2
+    ret
